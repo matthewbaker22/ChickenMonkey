0d12945e54d06eeae4006435eaea5ef5263be69d
Matthew Baker <mattbaker2281@gmail.com>
2020-01-17 11:13:18 -0600

    finished js for finding numbers which are multiples of other numbers

---
+++ main.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 100; i++) {
+    if (i % 5 === 0 && i % 7 === 0) {
+        console.log("ChickenMonkey");
+    } else if (i % 5 === 0) {
+        console.log("Chicken");
+    } else if (i & 7 === 0) {
+        console.log("Monkey");
+    } else {
+        console.log(i);
+    }
+}
